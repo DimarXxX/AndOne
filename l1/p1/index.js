f4f519bcda7d1952d3d50a6a16913a634f90f74c
Dzmitry Tupitsa <tdimasikt@gmail.com>
2018-07-14 14:59:25 +0300

    full first HomeWork

--- l1/p1/index.js
+++ l1/p1/index.js
@@ -4,17 +4,34 @@ let ch;
     let input = prompt ("User Name", "");
     let arr = input.split('');
     console.log(arr);
-    arr.forEach((item, i, arr) => {
-        if (isNaN(item)){
-            ch = true;
+
+    // Проверка массива на наличие чисел 
+    
+    let check = arr.every((item, i, arr) => {
+        if (isNaN(item)) {
+            return true;
         } else {
-           ch = false;
+            return false;
         }
     });
-    if (ch === true) {
-        document.write(arr.reverse())
+    //alert(check)
+
+
+    if (check === true) {
+        document.write(arr.reverse().join(''))
     } else {
-        document.write(arr + '   sd')
-    }
-}
+        // Создаем новую строку и меняем ей регистр в зависимости от индекса
+        let newStr = '';
+        for (let i = 0; i < input.length; i++) {
+            if (i %2 === 0) {
+              newStr += input[i].toUpperCase();
+            }
+            else {
+              newStr += input[i].toLowerCase();
+            }
+        }      
+             return document.write(newStr);
+        }
+        }
 getUserName();
+
